71886e2f8aecb78d2c04c73a2c86d99bebcd049e
rosko112 <aljazroser69@gmail.com>
2025-05-14 18:04:07 +0200

    zacetek
    Tu je samo zacetna stran ter direkotrij z slikami

---
+++ index.php
@@ -0,0 +1,41 @@
+<html lang="sl">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Airros</title>
+    </head>
+    <body>
+        <header>
+            <img src="slike/logo.png" alt="tukaj je slika">
+            <h1 class="naslov">AIRROS</h1>
+            <a href="#" class="prijava">Prijava</a>
+        </header>
+
+        <form>
+            <div class="iskanje">
+                <input type="option" placeholder="Od">
+                <input type="option" placeholder="do">
+                <input type="date" placeholder="Datum od">
+                <input type="date" placeholder="Datum do">
+                <input type="number" placeholder="Število potnikov" min="1" max="10"> <br>
+                <select>
+                    <option value="izbira">Izbira razreda</option>
+                    <option value="ekonomija">Ekonomija</option>
+                    <option value="prvi-razred">Prvi razred</option>
+                </select> <br>
+                <button type="submit" class="submit">Išči</button>
+                </div>
+        </form>
+        <footer>
+            <div class="footer">
+                <div class="footer-1">
+                    <h2>O nas</h2>
+                    <p>Naša zgodba se je začela pred več kot 20 leti, ko smo si zadali cilj, da bomo ljudem omogočili enostavno in udobno potovanje po svetu. Danes smo vodilno podjetje na področju letalskih prevozov in ponosni smo na našo tradicijo odličnosti.</p>
+                </div>
+                <div class="footer-2">
+                    <h2>Kontakt</h2>
+                    <p>Email: info@airros.com </p> 
+                    <p>Telefon: +386 40 123 456</p>
+            </div>  
+    </body>
+<html>
