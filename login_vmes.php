0ed4dde4d14d0202cc1a2c0f2ffb5b8137947329
rosko112 <aljazroser69@gmail.com>
2025-06-12 12:00:29 +0200

    final1

--- login_vmes.php
+++ login_vmes.php
@@ -4,27 +4,27 @@ session_start();
 
 if (isset($_POST['email']) && isset($_POST['geslo'])) {
     $mail = $_POST['email'];
-    $spremenjen_mail = htmlspecialchars($mail);
+    $spremenjen_mail = ($mail);
     $geslo = $_POST['geslo'];
-    $spremenjeno_geslo = htmlspecialchars($geslo);
+    $spremenjeno_geslo = ($geslo);
 
     $geslo1 = sha1($spremenjeno_geslo);
 
-    $sql = "SELECT id_u, vloga_id FROM uporabniki WHERE email = ? AND geslo = ?";
-    $stmt = mysqli_prepare($link, $sql);
+    $stmt = mysqli_prepare($link, "SELECT id_u, vloga_id FROM uporabniki WHERE email = ? AND geslo = ?");
     mysqli_stmt_bind_param($stmt, "ss", $mail, $geslo1);
     mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
+    mysqli_stmt_store_result($stmt);
 
-    if ($result) {
-        if ($row = mysqli_fetch_assoc($result)) {
+    if (mysqli_stmt_num_rows($stmt) == 1) {
+        mysqli_stmt_bind_result($stmt, $id_u, $vloga_id);
+        mysqli_stmt_fetch($stmt);
 
-            $_SESSION['email'] = $mail;
-            $_SESSION['id_u'] = $row['id_u'];
-            $_SESSION['vloga_id'] = $row['vloga_id'];
+        $_SESSION['email'] = $mail;
+        $_SESSION['id_u'] = $id_u;
+        $_SESSION['vloga_id'] = $vloga_id; 
 
-            header("Location: index.php");
-            exit;
+        header("Location: index.php");
+        exit;
     } else {
         $_SESSION['login_error'] = "Napačen email ali geslo.";
         header("Location: login.php");
@@ -32,6 +32,5 @@ if (isset($_POST['email']) && isset($_POST['geslo'])) {
     }
 
     mysqli_stmt_close($stmt);
-}
 }
 ?>
